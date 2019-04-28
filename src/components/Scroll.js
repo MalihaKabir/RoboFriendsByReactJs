20d5d6234bb14f5a411ecfffdc746e1cdf624ddd
MalihaKabir <malihakabir01@gmail.com>
2019-04-28 22:12:45 +0600

    border of Scroll element is removed

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Scroll = (props) => {
-    return (
-        <div style={{overflowY: 'scroll', border: '5px solid black', height: '700px'}}>
-            {props.children}
-        </div>
-    );
+	return (
+		// <div style={{overflowY: 'scroll', border: '5px solid black', height: '700px'}}>
+		<div style={{ overflowY: 'scroll', height: '700px' }}>
+			{props.children}
+		</div>
+	);
 };
 
 export default Scroll;
